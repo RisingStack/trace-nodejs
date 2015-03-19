9ff45444f8c210c28456f68e2e239cb1cf77bd7d
Gergely Nemeth <mail@nemethgergely.com>
2015-03-19 21:48:35 +0100

    Adding http request mock

--- seetru.js
+++ seetru.js
@@ -1,5 +1,18 @@
+var shimmer = require('./shimmer');
+
+var http = require('http');
+
 function seetru () {
 
+  shimmer.wrap(http, 'request', function (original) {
+    return function () {
+      console.log("Starting request!");
+      console.log(arguments[0]);
+      var returned = original.apply(this, arguments)
+      console.log("Done setting up request -- OH YEAH!");
+      return returned;
+    };
+  });
 }
 
-module.exports = seetru;
+seetru();
