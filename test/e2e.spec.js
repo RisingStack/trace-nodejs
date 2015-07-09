b19cc6ac12a40ada0817122a1b20d2984727058c
Gábor Döbrei <gabor.dobrei@gmail.com>
2015-07-09 11:00:19 +0200

    Call collector.setService from lib/index instead of reporter

--- test/e2e.spec.js
+++ test/e2e.spec.js
@@ -1,4 +1,3 @@
-var supertest = require('supertest');
 var expect = require('chai').expect;
 
 describe('The collector', function () {
@@ -17,14 +16,12 @@ describe('The collector', function () {
       throw new Error('Unhandled error');
     });
 
-    it('does not throw error if apiKey and appName is present', function () {
+    it('does not throw error if appName is present', function () {
       process.env.RISINGTRACE_APP_NAME = 'test';
-      process.env.RISINGTRACE_API_KEY = 'test';
 
-      var seetru = require('../lib');
+      require('../lib');
 
       process.env.RISINGTRACE_APP_NAME = undefined;
-      process.env.RISINGTRACE_API_KEY = undefined;
     });
 
   });
