b0fd036a90f0b56d49f10a05415682e3d17ce366
Gergely Nemeth <mail@nemethgergely.com>
2016-01-28 10:29:23 +0100

    fix(env): extending the default env, not overwriting

--- e2e/utils/childProcessTest.js
+++ e2e/utils/childProcessTest.js
@@ -4,13 +4,7 @@ var _ = require('lodash')
 var spawn = require('child_process').spawn
 
 function childProcessTest (testModule, options, callback) {
-  var env = {}
-  if (process.env.NODE_ENV) {
-    env.NODE_ENV = process.env.NODE_ENV
-  }
-  if (process.env.DEBUG) {
-    env.DEBUG = process.env_DEBUG
-  }
+  var env = process.env
   if (options && options.env) {
     _.extend(env, options.env)
   }
