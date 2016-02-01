2c43f56e64329f646daf2a87ba236697082b494a
Dávid Szakállas <david.szakallas@gmail.com>
2016-02-01 13:33:39 +0100

    feature(reporter): implement init-request-v2

--- e2e/getServiceKey.spec.js
+++ e2e/getServiceKey.spec.js
@@ -2,6 +2,7 @@
 
 var test = require('tape')
 var serviceMocks = require('./utils/serviceMocks')
+var pkg = require('../package.json')
 
 var TRACE_MODULE = '..'
 var TRACE_COLLECTOR_API_URL = 'https://trace-collector-api.risingstack.com'
@@ -12,7 +13,11 @@ test('should get service key', function (t) {
     TRACE_COLLECTOR_API_URL,
     TRACE_API_KEY_TEST,
     function (uri, requestBody) {
-      t.pass('gets service key')
+      t.equal(requestBody.name, process.env.TRACE_SERVICE_NAME, 'service name should be ok')
+      t.equal(requestBody.version, '2', 'version should be 2')
+      t.equal(requestBody.trace.version, pkg.version, 'package version should be ok')
+      t.ok(requestBody.runtime, "property 'runtime' should exist")
+      t.ok(requestBody.machine, "property 'machine' should exist")
       t.end()
       process.exit()
     })
