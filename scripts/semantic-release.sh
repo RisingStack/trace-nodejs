30079ac5f7e89bcc3caf5a73f08a7ea225cde29a
Dávid Szakállas <david.szakallas@gmail.com>
2016-11-11 11:41:45 +0100

    chore(scripts): fix release script

--- scripts/semantic-release.sh
+++ scripts/semantic-release.sh
@@ -5,9 +5,9 @@ source ./scripts/util/env-node.sh
 
 if [[ -z $IS_CI ]]; then
     echo "Not running in CI. Release skipped"
-elif [[ $PROJECT_REPONAME -ne $RELEASE_REPONAME ]]; then
+elif [ "$PROJECT_REPONAME" != "$RELEASE_REPONAME" ]; then
     echo "Project repo is not $RELEASE_REPONAME. Release skipped"
-elif [[ $CURRENT_BRANCH -ne $RELEASE_BRANCH ]]; then
+elif [ "$CURRENT_BRANCH" != "$RELEASE_BRANCH" ]]; then
     echo "Branch is not $RELEASE_BRANCH. Release skipped"
 else
     CI=true npm run semantic-release
