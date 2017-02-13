b5e9fa6499e09b09e08f939d6ad2e6ef9e8e0091
Dávid Szakállas <david.szakallas@gmail.com>
2017-02-13 15:07:57 +0100

    test(instrumentation): add more bluebird tests

--- test/instrumentations/promises/bluebird.spec.js
+++ test/instrumentations/promises/bluebird.spec.js
@@ -1,4 +1,12 @@
 'use strict'
 require('../test-setup.spec.js')
 
-require('./testPromise')('bluebird', require('bluebird'))
+var test = require('./testPromise')
+
+;['bluebird/js/release/promise', 'bluebird/js/main/promise'].forEach(function (name) {
+  var bb
+  try { bb = require(name) } catch (err) { }
+  if (bb) test(name, bb())
+})
+
+test('bluebird', require('bluebird'))
